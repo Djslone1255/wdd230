8b0bf12d80cb49317299adb0edf1f0fccb7a764e
Djslone1255 <djslone@byui.edu>
2024-01-17 00:03:41 -0800

    done

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,3 +1,7 @@
 // set current year in footer
 const currentDate = new Date();
 document.querySelector('#year').textContent = currentDate.getFullYear();
+
+// set last updated date in footer
+const lastupdated = new Date(document.lastModified);
+document.querySelector('#lastModified').textContent = lastupdated;
