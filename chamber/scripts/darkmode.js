a7bcc008f8fa693d43a4d6313516b5ca52ee9f70
Djslone1255 <djslone@byui.edu>
2024-05-30 20:36:33 -0700

    lesson 6

--- chamber/scripts/darkmode.js
+++ chamber/scripts/darkmode.js
@@ -1,8 +1,11 @@
-const modeButton = document.querySelector(".switch");
-const main = document.querySelector("main");
-const card = document.querySelector(".card");
+document.addEventListener('DOMContentLoaded', function() {
+    const modeButton = document.querySelector(".switch");
+    const main = document.querySelector("main");
+    const card = document.querySelector(".card");
 
-modeButton.addEventListener("click", () => {
-	main.classList.toggle('dark');
-    card.classList.toggle('dark');
+    modeButton.addEventListener("change", () => {
+        main.classList.toggle('dark');
+        card.classList.toggle('dark');
+    });
 });
+
